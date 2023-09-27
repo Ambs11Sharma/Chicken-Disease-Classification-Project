5dadb047aa2866317bb45db800c57818a4db64c0
Ambika2 Sharma <Ambika2.Sharma@ril.com>
2023-09-27 13:46:12 +0530

    requirements added

--- template.py
+++ template.py
@@ -21,7 +21,8 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "steup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
@@ -32,7 +33,7 @@ for filepath in list_of_files:
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filename) == 0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,"w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
